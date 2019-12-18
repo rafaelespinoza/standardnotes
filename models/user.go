f0787ba641b9dab76277b27e755bc9c32e8fe6ff
Rafael Espinoza <rafael@rafaelespinoza.com>
2019-12-17 21:42:14 -0800

    refactor: Re-arrange functions, methods in models.User
    Put all the method definitions with the same type. Also put exported
    methods before the unexported ones. Then declare the other types and
    define those methods.

--- models/user.go
+++ models/user.go
@@ -15,7 +15,6 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-// User is the user type
 type User struct {
 	UUID      string    `json:"uuid"`
 	Email     string    `json:"email"`
@@ -31,40 +30,8 @@ type User struct {
 	UpdatedAt time.Time `json:"updated_at"  sql:"updated_at"`
 }
 
-var _ db.MigratingUser = (*User)(nil)
-
-func (u *User) GetEmail() string   { return u.Email }
-func (u *User) GetPwNonce() string { return u.PwNonce }
-func (u *User) GetUUID() string    { return u.UUID }
-
-// Params is params type
-type Params struct {
-	PwFunc     string `json:"pw_func"     sql:"pw_func"`
-	PwAlg      string `json:"pw_alg"      sql:"pw_alg"`
-	PwCost     int    `json:"pw_cost"     sql:"pw_cost"`
-	PwKeySize  int    `json:"pw_key_size" sql:"pw_key_size"`
-	PwSalt     string `json:"pw_salt"     sql:"pw_salt"`
-	PwNonce    string `json:"pw_nonce"    sql:"pw_nonce"`
-	Version    string `json:"version"`
-	Identifier string `json:"identifier"` // should be email address
-}
-
-// NewPassword - incomming json password change
-type NewPassword struct {
-	User
-	CurrentPassword string `json:"current_password"`
-	NewPassword     string `json:"new_password"`
-}
-
-// UserClaims - jwt claims
-type UserClaims struct {
-	UUID   string `json:"uuid"`
-	PwHash string `json:"pw_hash"`
-	jwt.StandardClaims
-}
-
-//NewUser - user constructor
-func NewUser() User {
+// NewUser initializes a User with default values.
+func NewUser() *User {
 	user := User{}
 	user.PwCost = 100000
 	user.PwAlg = "sha512"
@@ -72,10 +39,16 @@ func NewUser() User {
 	user.PwFunc = "pbkdf2"
 	user.CreatedAt = time.Now()
 	user.UpdatedAt = time.Now()
-	return user
+	return &user
 }
 
-//LoadValue - hydrate struct from map
+var _ db.MigratingUser = (*User)(nil)
+
+func (u *User) GetEmail() string   { return u.Email }
+func (u *User) GetPwNonce() string { return u.PwNonce }
+func (u *User) GetUUID() string    { return u.UUID }
+
+// LoadValue hydrates a struct from a map.
 func (u *User) LoadValue(name string, value []string) {
 	switch name {
 	case "uuid":
@@ -101,7 +74,141 @@ func (u *User) LoadValue(name string, value []string) {
 	}
 }
 
-//save - save current user into DB
+// UpdatePassword updates the user's password.
+func (u *User) UpdatePassword(np NewPassword) error {
+	if u.UUID == "" {
+		return fmt.Errorf("Unknown user")
+	}
+
+	u.Password = Hash(np.NewPassword)
+	u.PwCost = np.PwCost
+	u.PwSalt = np.PwSalt
+	u.PwNonce = np.PwNonce
+
+	u.UpdatedAt = time.Now()
+	// TODO: validate incoming password params
+	err := db.Query("UPDATE `users` SET `password`=?, `pw_cost`=?, `pw_salt`=?, `pw_nonce`=?, `updated_at`=? WHERE `uuid`=?", u.Password, u.PwCost, u.PwSalt, u.PwNonce, u.UpdatedAt, u.UUID)
+
+	if err != nil {
+		logger.Log(err)
+		return err
+	}
+
+	return nil
+}
+
+// UpdateParams - update params
+func (u *User) UpdateParams(p Params) error {
+	if u.UUID == "" {
+		return fmt.Errorf("Unknown user")
+	}
+
+	u.UpdatedAt = time.Now()
+	err := db.Query("UPDATE `users` SET `pw_func`=?, `pw_alg`=?, `pw_cost`=?, `pw_key_size`=?, `pw_salt`=?, `updated_at`=? WHERE `uuid`=?", u.PwFunc, u.PwAlg, u.PwCost, u.PwKeySize, u.PwSalt, time.Now(), u.UUID)
+
+	if err != nil {
+		logger.Log(err)
+		return err
+	}
+
+	return nil
+}
+
+// Register creates a new user and returns a token.
+func (u *User) Register() (string, error) {
+	err := u.create()
+	if err != nil {
+		return "", err
+	}
+
+	token, err := u.Login(u.Email, u.Password)
+	if err != nil {
+		return "", fmt.Errorf("Registration failed")
+	}
+
+	return token, nil
+}
+
+// Exists checks if the user exists in the DB.
+func (u User) Exists() bool {
+	uuid, err := db.SelectFirst("SELECT `uuid` FROM `users` WHERE `email`=?", u.Email)
+
+	if err != nil {
+		logger.Log(err)
+		return false
+	}
+
+	return uuid != ""
+}
+
+// Login signs in the user. It returns a token on success, otherwise an error.
+func (u *User) Login(email, password string) (string, error) {
+	u.loadByEmailAndPassword(email, Hash(password))
+
+	if u.UUID == "" {
+		return "", fmt.Errorf("Invalid email or password")
+	}
+
+	token, err := u.CreateToken()
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+// LoadByUUID hydrates the user from the DB.
+func (u *User) LoadByUUID(uuid string) bool {
+	_, err := db.SelectStruct(fmt.Sprintf("SELECT %s FROM `users` WHERE `uuid`=?", sqlstruct.Columns(User{})), u, uuid)
+	if err != nil {
+		logger.Log("Load err:", err)
+		return false
+	}
+
+	return true
+}
+
+// CreateToken makes a JWT token.
+func (u User) CreateToken() (string, error) {
+	claims := UserClaims{
+		u.UUID,
+		u.Password,
+		jwt.StandardClaims{
+			IssuedAt: time.Now().Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(encryption.SigningKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// Validate checks the jwt for a valid password.
+func (u User) Validate(password string) bool {
+	return password == u.Password
+}
+
+// ToJSON - return map without pw and nonce
+func (u User) ToJSON() interface{} { // TODO: rm this method
+	u.Password = ""
+	u.PwNonce = ""
+	return u
+}
+
+// LoadByEmail populates the user fields with a DB lookup.
+func (u *User) LoadByEmail(email string) error {
+	_, err := db.SelectStruct("SELECT * FROM `users` WHERE `email`=?", u, email)
+	if err != nil {
+		logger.Log(err)
+	}
+	return err
+}
+
+// create saves the user to the DB.
 func (u *User) create() error {
 	if u.UUID != "" {
 		return fmt.Errorf("Trying to save existing user")
@@ -126,128 +233,6 @@ func (u *User) create() error {
 		logger.Log(err)
 	}
 
-	return err
-}
-
-//UpdatePassword - update password
-func (u *User) UpdatePassword(np NewPassword) error {
-	if u.UUID == "" {
-		return fmt.Errorf("Unknown user")
-	}
-
-	u.Password = Hash(np.NewPassword)
-	u.PwCost = np.PwCost
-	u.PwSalt = np.PwSalt
-	u.PwNonce = np.PwNonce
-
-	u.UpdatedAt = time.Now()
-	// TODO: validate incomming pw params
-	err := db.Query("UPDATE `users` SET `password`=?, `pw_cost`=?, `pw_salt`=?, `pw_nonce`=?, `updated_at`=? WHERE `uuid`=?", u.Password, u.PwCost, u.PwSalt, u.PwNonce, u.UpdatedAt, u.UUID)
-
-	if err != nil {
-		logger.Log(err)
-		return err
-	}
-
-	return nil
-}
-
-//UpdateParams - update params
-func (u *User) UpdateParams(p Params) error {
-	if u.UUID == "" {
-		return fmt.Errorf("Unknown user")
-	}
-
-	u.UpdatedAt = time.Now()
-	err := db.Query("UPDATE `users` SET `pw_func`=?, `pw_alg`=?, `pw_cost`=?, `pw_key_size`=?, `pw_salt`=?, `updated_at`=? WHERE `uuid`=?", u.PwFunc, u.PwAlg, u.PwCost, u.PwKeySize, u.PwSalt, time.Now(), u.UUID)
-
-	if err != nil {
-		logger.Log(err)
-		return err
-	}
-
-	return nil
-}
-
-//Register - creates user and returns token
-func (u *User) Register() (string, error) {
-	err := u.create()
-	if err != nil {
-		return "", err
-	}
-
-	token, err := u.Login(u.Email, u.Password)
-	if err != nil {
-		return "", fmt.Errorf("Registration failed")
-	}
-
-	return token, nil
-}
-
-//Exists - checks if current user exists in DB
-func (u User) Exists() bool {
-	uuid, err := db.SelectFirst("SELECT `uuid` FROM `users` WHERE `email`=?", u.Email)
-
-	if err != nil {
-		logger.Log(err)
-		return false
-	}
-
-	return uuid != ""
-}
-
-//Login - logins user
-func (u *User) Login(email, password string) (string, error) {
-	u.loadByEmailAndPassword(email, Hash(password))
-
-	if u.UUID == "" {
-		return "", fmt.Errorf("Invalid email or password")
-	}
-
-	token, err := u.CreateToken()
-	if err != nil {
-		return "", err
-	}
-
-	return token, nil
-}
-
-//LoadByUUID - loads user info from DB
-func (u *User) LoadByUUID(uuid string) bool {
-	_, err := db.SelectStruct(fmt.Sprintf("SELECT %s FROM `users` WHERE `uuid`=?", sqlstruct.Columns(User{})), u, uuid)
-	if err != nil {
-		logger.Log("Load err:", err)
-		return false
-	}
-
-	return true
-}
-
-//CreateToken - will create JWT token
-func (u User) CreateToken() (string, error) {
-	claims := UserClaims{
-		u.UUID,
-		u.Password,
-		jwt.StandardClaims{
-			IssuedAt: time.Now().Unix(),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(encryption.SigningKey)
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, nil
-}
-
-// LoadByEmail populates the user fields with a DB lookup.
-func (u *User) LoadByEmail(email string) error {
-	_, err := db.SelectStruct("SELECT * FROM `users` WHERE `email`=?", u, email)
-	if err != nil {
-		logger.Log(err)
-	}
 	return err
 }
 
@@ -306,19 +291,26 @@ func MakeAuthParams(u User) Params {
 	return params
 }
 
-//Validate - validates password from jwt
-func (u User) Validate(password string) bool {
-	return password == u.Password
+// NewPassword helps facilitate user password changes.
+type NewPassword struct {
+	User
+	CurrentPassword string `json:"current_password"`
+	NewPassword     string `json:"new_password"`
 }
 
-//ToJSON - return map without pw and nonce
-func (u User) ToJSON() interface{} {
-	u.Password = ""
-	u.PwNonce = ""
-	return u
+// UserClaims is a set of JWT claims.
+type UserClaims struct {
+	UUID   string `json:"uuid"` // TODO: verify that this is User.UUID
+	PwHash string `json:"pw_hash"`
+	jwt.StandardClaims
 }
 
-//Hash - sha256 hash function
+// Hash computes a sha256 checksum of the input.
 func Hash(input string) string {
-	return strings.Replace(fmt.Sprintf("% x", sha256.Sum256([]byte(input))), " ", "", -1)
+	return strings.Replace(
+		fmt.Sprintf("% x", sha256.Sum256([]byte(input))),
+		" ",
+		"",
+		-1,
+	)
 }
